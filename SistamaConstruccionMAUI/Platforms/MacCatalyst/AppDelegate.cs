d31d518908f697909aec18be7b2fa64f4535bd04
Thiago Vale <163936602+ThiagoVaale@users.noreply.github.com>
2026-02-12 17:35:57 -0300

    Initial clean arquitecture setup

---
+++ SistamaConstruccionMAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace SistamaConstruccionMAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
